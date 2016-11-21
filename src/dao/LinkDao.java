f1cabd3a2dae6c80aa1ee2996e024145d419cbdb
Amar <amar.poddar@dtdc.com>
2016-11-21 10:18:16 +0530

    Line added in LinkDao.java file from Locacl branch

--- src/dao/LinkDao.java
+++ src/dao/LinkDao.java
@@ -11,7 +11,7 @@ import entity.Link;
  * @author milandobrota
  */
 public interface LinkDao extends GenericDao<Link, Integer> {
-    
+    chnages doen in my another local branch
     public String test();
 
     public Link createLink(String link, Integer postId);
